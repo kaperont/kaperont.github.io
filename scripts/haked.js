edbf318dec6968c31398e49bdbc44ecb3e027ad1
kaperont <kperont108@cedarville.edu>
2022-03-21 22:39:43 -0400

    Initial Commit

---
+++ scripts/haked.js
@@ -0,0 +1,13 @@
+function display(src, width, height, alt) {
+    var a = document.createElement("img");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    a.display = "block";
+    a.style.margin = "0 auto";
+    a.style.top = "100px";
+    // document.body.style.backgroundImage = "url('https://JackWilliamson116.github.io/assets/scary-face.png')";
+    document.body.appendChild(a);
+}
+display('https://JackWilliamson116.github.io/assets/scary-face.png', 1000, 1000, 'Dr. Nefario')
