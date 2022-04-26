cfad424c46783d9ed28bbc8ff12cc3db4dd01923
kaperont <kperont108@cedarville.edu>
2022-04-26 12:14:48 -0400

    notahack

--- scripts/notahack.js
+++ scripts/notahack.js
@@ -1,25 +1,30 @@
 function display(src, width, height, alt) {
-    // var a = document.createElement("img");
-    // a.src = src;
-    // a.width = width;
-    // a.height = height;
-    // a.alt = alt;
-    // a.display = "block";
-    // a.style.margin = "0 auto";
-    // a.style.top = "100px";
-    // document.body.appendChild(a);
+    var a = document.createElement("img");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    a.display = "block";
+    a.style.margin = "0 auto";
+    a.style.top = "100px";
+    document.body.appendChild(a);
 
     // // Load some scary audio bro
     var source = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     var video = document.createElement("video");
     //
+    video.src = source;
     video.autoplay = true;
+    video.controls = true;
+    video.muted = false;
+    video.height = 240;
+    video.width = 320;
     //
     video.load()
     video.addEventListener("load", function() { 
         video.play(); 
     }, true);
-    video.src = source;
+    
     document.body.appendChild(video);
     document.activeElement.appendChild(video);
     
